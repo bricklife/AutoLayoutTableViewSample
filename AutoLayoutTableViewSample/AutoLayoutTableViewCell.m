057afc0709fed47bbdb8eb8b93a9f2baf794a952
Shinichiro Oba <ooba@bricklife.com>
2014-12-12 00:41:20 +0900

    Improve layout logic

--- AutoLayoutTableViewSample/AutoLayoutTableViewCell.m
+++ AutoLayoutTableViewSample/AutoLayoutTableViewCell.m
@@ -24,6 +24,10 @@
     [super layoutSubviews];
     
     if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+        // コンテナビューを再レイアウト
+        [self.contentView setNeedsLayout];
+        [self.contentView layoutIfNeeded];
+
         // preferredMaxLayoutWidth を自身の幅と同じにする
         // iOS 8のときは 0 のままにしておけば自動計算してくれるので必要ない
         self.label.preferredMaxLayoutWidth = CGRectGetWidth(self.label.frame);
